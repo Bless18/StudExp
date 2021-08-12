a566f42c30c2453191e76d626f5ec2bb84806cc2
Bless18 <mbohblesspearl@gmail.com>
2021-08-12 11:55:29 +0100

    StudExp to The World

--- app/src/main/java/com/bless/studexp/ui/Subjects/SubjectsFragment.java
+++ app/src/main/java/com/bless/studexp/ui/Subjects/SubjectsFragment.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -19,9 +20,14 @@ import com.bless.studexp.Adapters.SubjectRecyclerAdapter;
 import com.bless.studexp.databinding.FragmentSubjectsBinding;
 import com.bless.studexp.models.Course;
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SubjectsFragment extends Fragment implements SubjectRecyclerAdapter.OnCourseListener {
 
@@ -46,7 +52,19 @@ public class SubjectsFragment extends Fragment implements SubjectRecyclerAdapter
                  binding.shimmerFrameLayout.setVisibility(View.GONE);
                  binding.shimmerImageSelected.stopShimmer();
                  binding.shimmerImageSelected.setVisibility(View.GONE);
+
+             }
+         });
+         binding.testUpload.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
                  db = FirebaseFirestore.getInstance();
+                 AddDataToCloudStore(new Course("Phylosophy",
+                         "Art",
+                         "the study of theology",
+                         "https://ichef.bbci.co.uk/news/976/cpsprodpb/A72C/production/_118569724_gettyimages-1130153707.jpg0",
+                         "University"
+                 ));
              }
          });
             return  root;
@@ -72,6 +90,27 @@ public class SubjectsFragment extends Fragment implements SubjectRecyclerAdapter
         Glide.with(getContext()).load(imageUrl).into(binding.imageBroad);
     }
     private void AddDataToCloudStore(Course course){
+        Map<String,String> courseUpdate=new HashMap<>();
+        courseUpdate.put("name",course.getName());
+        courseUpdate.put("category",course.getCategory());
+        courseUpdate.put("description",course.getDescription());
+        courseUpdate.put("imageUrl",course.getImageUrl());
+        courseUpdate.put("level",course.getLevel());
 
+        db.collection("Courses/University/1")
+                .add(courseUpdate)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Toast.makeText(getActivity(), "Successfully uploaded", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG, "onFailure: Load Failure");
+                    }
+                });
+        
     }
 }
